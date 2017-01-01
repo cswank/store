f44e54e7db0e54d9bd174aa8d60e3a3e2749b0ce
Craig Swank <craigswank@gmail.com>
2017-01-01 11:52:19 -0700

    wip

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,16 +1,23 @@
 package config
 
 type Config struct {
-	HashKey          string   `env:"STORE_HASH_KEY" envDefault:"we all live in a"`
-	BlockKey         string   `env:"STORE_BLOCK_KEY" envDefault:"yellow submarine"`
-	LogOutput        string   `env:"STORE_LOG_OUTPUT" envDefault:"stdout"`
-	DataDir          string   `env:"STORE_DATADIR" envDefault:"/var/log/store"`
-	DefaultPrice     string   `env:"STORE_DEFAULT_PRICE" envDefault:"0.00"`
-	StoreName        string   `env:"STORE_DEFAULT_NAME" envDefault:"example"`
-	CaptchaURL       string   `env:"STORE_RECAPTCHA_URL" envDefault:"example"`
-	CaptchaSecretKey string   `env:"STORE_RECAPTCHA_SECRET_KEY" envDefault:""`
-	CaptchaSiteKey   string   `env:"STORE_RECAPTCHA_SITE_KEY" envDefault:""`
-	Iface            string   `env:"STORE_IFACE" envDefault:"127.0.0.1"`
-	Port             int      `env:"STORE_PORT" envDefault:"8080"`
-	Domains          []string `env:"STORE_DOMAINS" envDefault:"localhost"`
+	CookieBlockKey     string   `env:"STORE_BLOCK_KEY" envDefault:"yellow submarine"`
+	CookieHashKey      string   `env:"STORE_HASH_KEY" envDefault:"we all live in a"`
+	DataDir            string   `env:"STORE_DATADIR" envDefault:"/var/log/store"`
+	DefaultPrice       string   `env:"STORE_DEFAULT_PRICE" envDefault:"0.00"`
+	Domains            []string `env:"STORE_DOMAINS" envDefault:"localhost"`
+	Email              string   `env:"STORE_EMAIL" envDefault:"support@localhost"`
+	EmailPassword      string   `env:"STORE_EMAIL_PASSWORD" envDefault:"hushhush"`
+	Iface              string   `env:"STORE_IFACE" envDefault:"127.0.0.1"`
+	LogOutput          string   `env:"STORE_LOG_OUTPUT" envDefault:"stdout"`
+	Port               int      `env:"STORE_PORT" envDefault:"8080"`
+	RecaptchaSecretKey string   `env:"STORE_RECAPTCHA_SECRET_KEY" envDefault:""`
+	RecaptchaSiteKey   string   `env:"STORE_RECAPTCHA_SITE_KEY" envDefault:""`
+	RecaptchaURL       string   `env:"STORE_RECAPTCHA_URL" envDefault:"https://www.google.com/recaptcha/api/siteverify"`
+	ShopifyAPI         string   `env:"STORE_SHOPIFY_API"`
+	ShopifyDomain      string   `env:"STORE_SHOPIFY_DOMAIN"`
+	ShopifyJSKey       string   `env:"STORE_SHOPIFY_JS_KEY"`
+	StoreName          string   `env:"STORE_NAME" envDefault:"example"`
+	TLSCerts           string   `env:"STORE_TLS_CERTS"`
+	UseTLS             bool     `env:"STORE_USE_TLS" envDefault:"false"`
 }
