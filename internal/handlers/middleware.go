63277dacb6314e8a00b4dc45434017059c57bdcf
Craig Swank <craigswank@gmail.com>
2017-01-20 18:08:36 -0700

    log request url

--- internal/handlers/middleware.go
+++ internal/handlers/middleware.go
@@ -13,28 +13,15 @@ var (
 	lg      *log.Logger
 )
 
-func getLogger(logOutput string) *log.Logger {
-	f := log.Ldate | log.Ltime | log.Lmicroseconds
-	if logOutput == "stdout" {
-		return log.New(os.Stdout, "", f)
-	}
-
-	var err error
-	if logFile, err = os.Create("/tmp/store.log"); err != nil {
-		log.Fatal(err)
-	}
-	return log.New(logFile, "", f)
-}
-
 func Close() {
 	logFile.Close()
 }
 
 func Log(logOutput string) alice.Constructor {
-	lg = getLogger(logOutput)
+	lg = log.New(os.Stdout, "", 0)
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			lg.Println(req.Method, req.URL.Path)
+			lg.Println(req.RemoteAddr, req.Method, req.URL.Path)
 			h.ServeHTTP(w, req)
 		})
 	}
